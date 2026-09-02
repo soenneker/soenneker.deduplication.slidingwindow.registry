259a5107f18de80eb13fadb484cbd727ab20f3b6
soenneker <jake@soenneker.com>
2026-09-02 14:22:05 -0500

    Add cancellation tokens to async tests

--- test/Soenneker.Deduplication.SlidingWindow.Registry.Tests/SlidingWindowDedupeRegistryTests.cs
+++ test/Soenneker.Deduplication.SlidingWindow.Registry.Tests/SlidingWindowDedupeRegistryTests.cs
@@ -1,3 +1,4 @@
+using System.Threading;
 using System;
 using System.Threading.Tasks;
 using AwesomeAssertions;
@@ -18,15 +19,15 @@ public sealed class SlidingWindowDedupeRegistryTests : HostedUnitTest
     }
 
     [Test]
-    public async Task Remove_discards_history_and_configuration()
+    public async Task Remove_discards_history_and_configuration(CancellationToken cancellationToken)
     {
-        ISlidingWindowDedupe first = await _util.Get("scope-remove", TimeSpan.FromMinutes(5), TimeSpan.FromSeconds(10));
+        ISlidingWindowDedupe first = await _util.Get("scope-remove", TimeSpan.FromMinutes(5), TimeSpan.FromSeconds(10), cancellationToken: cancellationToken);
         first.TryMarkSeen("item-1").Should().BeTrue();
 
-        (await _util.Remove("scope-remove")).Should().BeTrue();
+        (await _util.Remove("scope-remove", cancellationToken: cancellationToken)).Should().BeTrue();
         _util.TryGet("scope-remove", out _).Should().BeFalse();
 
-        ISlidingWindowDedupe replacement = await _util.Get("scope-remove", TimeSpan.FromMinutes(1), TimeSpan.FromSeconds(5));
+        ISlidingWindowDedupe replacement = await _util.Get("scope-remove", TimeSpan.FromMinutes(1), TimeSpan.FromSeconds(5), cancellationToken: cancellationToken);
         replacement.Should().NotBeSameAs(first);
         replacement.TryMarkSeen("item-1").Should().BeTrue();
     }
